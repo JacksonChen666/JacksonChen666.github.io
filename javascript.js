d0baed5e17cde5bfbcaf1d42104509aad985cdbf
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-06 17:11:01 +0100

    Automated commit and push

--- javascript.js
+++ javascript.js
@@ -19,3 +19,38 @@ function hnav() {
 function easterEgg() {
 	document.write("no way")
 }
+
+function billWurtzQuestionsJS() {    
+    var question_submitted_already = false;     
+    function submit_question() {
+        if (question_submitted_already == false) 
+        {
+            question_submitted_already = true;
+            document.forms["ask"].submit(); 
+        }
+    }
+        
+        
+    function on_press(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        
+        // check for orientation field on mobile 
+         if (!(typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
+            var question = document.getElementsByName("question")[0].value;
+             
+            // if u press enter and no shift, send q
+            if (code == 13 && !e.shiftKey && question.length < CHARACTERS_TO_DISALLOW_ENTER) { 
+        
+                submit_question(); 
+                e.preventDefault();
+
+            }
+         }
+    }
+	
+          //does not refresh the page!!!! on safari back or reload of page make the question not submitted
+         window.addEventListener('pageshow', function(e) {
+            question_submitted_already = false;
+             document.getElementsByName("question")[0].value = "";
+        });
+}
