25c6c1388749b7f683e286f1b5636d2a555be08b
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-08 13:48:56 +0100

    Automated commit and push

--- history/2020/03/06/published/questions.php
+++ history/2020/03/06/published/questions.php
@@ -7,28 +7,27 @@
 </head>
 <body>
 
-<h1>processing..</h1>
+<h1>processing q..</h1>
 <!-- <button onclick="goBack()">back</button> -->
 <p style="font-size: 5px">(if you see undefined index errors you shouldn't be here)</p>
+<p><a href="https://github.com/JacksonChen666/JacksonChen666.github.io/issues/new?assignees=&labels=bug&template=bug_report.md&title=question.php%20Problems">Report bugs on this page (errors, and other stuff)</a>
 
 <?php
 $txt = $_POST["question"];
-if($_POST['formSubmit'] == "ASK") {
-	$file = fopen("questions.txt", "a") or die("can't open file F");
-	// echo "file open\n";
-	fwrite($file, $txt);
-	fwrite($file, "\n");
-	// echo "q written\n";
-	fclose($file);
-	// echo "file closed\n";
-	echo "ok done, you can go back :)";
-}
 if(empty($txt)) {
 	echo "you must actually ask >:(";
+}
+else {
+	$file = fopen("questions.txt", "a") or die("can't open file F");
+	fwrite($file, time());
+	fwrite($file, ": ");
+	fwrite($file, $txt);
+	fwrite($file, "\n\n");
+	fclose($file);
+	echo "ok done, you can go back :)";
 }
 ?>
 
 <p><a href="https://JacksonChen666.github.io/published/questions.html">Go back to questions</a></p>
-
 </body>
 </html>
