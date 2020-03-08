654566bfe4315b0fbe88ed4ea5d8925eb43229cd
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-08 22:16:43 +0100

    fixes

--- development/questions.php
+++ development/questions.php
@@ -9,13 +9,12 @@
 
 <h1>processing q..</h1>
 <!-- <button onclick="goBack()">back</button> -->
-<p style="font-size: 5px">(if you see undefined index errors you shouldn't be here)</p>
 <p><a href="https://github.com/JacksonChen666/JacksonChen666.github.io/issues/new?assignees=&labels=bug&template=bug_report.md&title=question.php%20Problems">Report bugs on this page (errors, and other stuff)</a>
 
 <?php
 $txt = $_POST["question"];
 if(empty($txt)) {
-	echo "you must actually ask >:(";
+	echo "you must actually ask ノಠ益ಠノ彡┻━┻";
 }
 else {
 	$file = fopen("questions.txt", "a") or die("can't open file F");
@@ -24,10 +23,10 @@ else {
 	fwrite($file, $txt);
 	fwrite($file, "\n\n");
 	fclose($file);
-	echo "ok done, you can go back :)";
+	echo "done";
 }
 ?>
-
 <p><a href="https://JacksonChen666.github.io/published/questions.html">Go back to questions</a></p>
+<p style="font-size= 3px">\(°_o)/¯</p>
 </body>
 </html>
