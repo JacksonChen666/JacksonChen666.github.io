0ec274217627412d11e854476527501bb6f841be
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-11 17:04:49 +0100

    extra testing

--- php/questions.php
+++ php/questions.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<h1>processing q..</h1>
+<!-- <h1>processing q..</h1> -->
 <!-- <button onclick="goBack()">back</button> -->
 
 <?php
@@ -29,8 +29,8 @@ else {
 	echo "done";
 }
 ?>
-<p><a href="https://JacksonChen666.github.io/questions.html">Go back to questions</a></p>
+<!-- <p><a href="https://JacksonChen666.github.io/questions.html">Go back to questions</a></p>
 <p><a href="https://github.com/JacksonChen666/JacksonChen666.github.io/issues/new?assignees=&labels=bug&template=bug_report.md&title=question.php%20Problems">Report bugs on this page (errors, and other stuff)</a></p><br><br>
-<p style="font-size= 3px">\(°_o)/¯</p>
+<p style="font-size= 3px">\(°_o)/¯</p> -->
 </body>
 </html>
