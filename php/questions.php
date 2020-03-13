63dd7973c2afacb8a2dd1c775f7c8df69d5b3389
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-14 00:23:21 +0100

    hope this thing works

--- php/questions.php
+++ php/questions.php
@@ -11,21 +11,31 @@
 <!-- <button onclick="goBack()">back</button> -->
 
 <?php
+
 $txt = $_POST["question"];
-$URLhate = "https://JacksonChen666.github.io/must-do-it.html";
-$URLthank = "https://JacksonChen666.github.io/thank-you.html";
-if(empty($txt)) {
-	echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URLhate . '">';
+
+function redirectBack($URLToRedirect = "") {
+	echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URLToRedirect . '">';
 }
-else {
+
+function writeQ($question) {
 	$file = fopen("questions.txt", "a") or die("can't open file F");
 	fwrite($file, time());
 	fwrite($file, ": ");
-	fwrite($file, $txt);
+	fwrite($file, $question);
 	fwrite($file, "\n\n");
 	fclose($file);
-	echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URLthank . '">';
+	redirectBack("https://JacksonChen666.github.io/thank-you.html")
 }
+
+if(empty($txt)) {
+	redirectBack("https://JacksonChen666.github.io/must-do-it.html")
+}
+
+else {
+	writeQ($txt);
+}
+
 ?>
 <!-- <p><a href="https://JacksonChen666.github.io/questions.html">Go back to questions</a></p>
 <p><a href="https://github.com/JacksonChen666/JacksonChen666.github.io/issues/new?assignees=&labels=bug&template=bug_report.md&title=question.php%20Problems">Report bugs on this page (errors, and other stuff)</a></p><br><br>
