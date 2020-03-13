7f20452f5249ae2d0483abcde0c942ee8225c97d
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-13 17:38:09 +0100

    oooo animations

---
+++ animations.js
@@ -0,0 +1,68 @@
+function leaveAnimation() {
+    document.getElementById('all-animation').className = 'all-animation';
+}
+
+function clickLinkHome() {
+    leaveAnimation();
+    window.location.href = "./";
+}
+
+function clickLinkLife() {
+    leaveAnimation();
+    window.location.href = "./life.html";
+}
+
+function clickLinkQs() {
+    leaveAnimation();
+    window.location.href = "./questions.html";
+}
+
+function clickLinkCoding() {
+    leaveAnimation();
+    window.location.href = "./coding.html";
+}
+
+function clickLinkSM() {
+    leaveAnimation();
+    window.location.href = "./social-media.html";
+}
+
+function clickLinkMemes() {
+    leaveAnimation();
+    window.location.href = "./memes.html";
+}
+
+function clickLinkNotes() {
+    leaveAnimation();
+    window.location.href = "./notes.html";
+}
+
+function clickLinkIndex() {
+    leaveAnimation();
+    window.location.href = "./site-index.html";
+}
+
+function clickLinkOldQs() {
+    leaveAnimation();
+    window.location.href = "./old-stuff/old-questions.html";
+}
+
+function clickLinkAS() {
+    leaveAnimation();
+    window.location.href = "./apple-script.html";
+}
+
+function clickLinkIdeas() {
+    leaveAnimation();
+    window.location.href = "./ideas.html";
+}
+
+function clickLinkOldIdeas() {
+    leaveAnimation();
+    window.location.href = "./old-stuff/old-ideas.html";
+}
+
+function clickLinkAnimationTesting() {
+    leaveAnimation();
+    window.location.href = "./animation-testing.html";
+}
