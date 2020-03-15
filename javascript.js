d5660f3f4d017bba0ac60d8227b94c8560031279
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-15 15:39:10 +0100

    reduced javascript

--- javascript.js
+++ javascript.js
@@ -1,78 +1,78 @@
-function test() {
-	document.findElementById("").innerHTML = ""
-}
-
 function goBack() {
 	window.history.back()
 }
 
-function autoRefresh() {
-    if (sessionStorage.getItem("refresh") == "true") {
-        sessionStorage.removeItem("refresh"); window.location.reload()
-    }
-}
+// function autoRefresh() {
+//     if (sessionStorage.getItem("refresh") == "true") {
+//         sessionStorage.removeItem("refresh"); window.location.reload()
+//     }
+// }
 
-function redirectWaitThenShow() {
-	// sleep(3);
-	document.getElementById("homeR").innerHTML = "Redirecting...";
-	document.getElementById("homeR3").innerHTML = "Click here if you aren't being redirected";
-}
+// function test() {
+//     document.findElementById("").innerHTML = ""
+// }
 
-function hnav() {
-	  $("#hnav").load("header-and-nav.html"); 
-}
+// function redirectWaitThenShow() {
+// 	// sleep(3);
+// 	document.getElementById("homeR").innerHTML = "Redirecting...";
+// 	document.getElementById("homeR3").innerHTML = "Click here if you aren't being redirected";
+// }
 
-function easterEgg() {
-	document.write("no way");
-}
+// function hnav() {
+// 	  $("#hnav").load("header-and-nav.html"); 
+// }
 
-function loadAlert() {
-    alert("Loaded element");
-}
+// function easterEgg() {
+// 	document.write("no way");
+// }
 
-function forms() {
-    var question_submitted_already = false;     
-        if (question_submitted_already == false) 
-        {
-            question_submitted_already = true;
-            document.forms["ask"].submit(); 
-        }
-        window.addEventListener('pageshow', function(e) {
-            question_submitted_already = false;
-        });
-}
+// function loadAlert() {
+//     alert("Loaded element");
+// }
 
-function billWurtzQuestionsJS() {
-    var question_submitted_already = false;     
-    function submit_question() {
-        if (question_submitted_already == false) 
-        {
-            question_submitted_already = true;
-            document.forms["ask"].submit(); 
-        }
-    }
+// function forms() {
+//     var question_submitted_already = false;     
+//         if (question_submitted_already == false) 
+//         {
+//             question_submitted_already = true;
+//             document.forms["ask"].submit(); 
+//         }
+//         window.addEventListener('pageshow', function(e) {
+//             question_submitted_already = false;
+//         });
+// }
+
+// function billWurtzQuestionsJS() {
+//     var question_submitted_already = false;     
+//     function submit_question() {
+//         if (question_submitted_already == false) 
+//         {
+//             question_submitted_already = true;
+//             document.forms["ask"].submit(); 
+//         }
+//     }
         
         
-    function on_press(e) {
-        var code = (e.keyCode ? e.keyCode : e.which);
+//     function on_press(e) {
+//         var code = (e.keyCode ? e.keyCode : e.which);
         
-        // check for orientation field on mobile 
-         if (!(typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
-            var question = document.getElementsByName("question")[0].value;
+//         // check for orientation field on mobile 
+//          if (!(typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
+//             var question = document.getElementsByName("question")[0].value;
              
-            // if u press enter and no shift, send q
-            if (code == 13 && !e.shiftKey && question.length < CHARACTERS_TO_DISALLOW_ENTER) { 
+//             // if u press enter and no shift, send q
+//             if (code == 13 && !e.shiftKey && question.length < CHARACTERS_TO_DISALLOW_ENTER) { 
         
-                submit_question(); 
-                e.preventDefault();
+//                 submit_question(); 
+//                 e.preventDefault();
 
-            }
-         }
-    }
+//             }
+//          }
+//     }
 	
-          //does not refresh the page!!!! on safari back or reload of page make the question not submitted
-         window.addEventListener('pageshow', function(e) {
-            question_submitted_already = false;
-             document.getElementsByName("question")[0].value = "";
-        });
-}
+//           //does not refresh the page!!!! on safari back or reload of page make the question not submitted
+//          window.addEventListener('pageshow', function(e) {
+//             question_submitted_already = false;
+//              document.getElementsByName("question")[0].value = "";
+//         });
+// }
