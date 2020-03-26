c986be60c0980979e2e4acfb1db66f98df00a6d6
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-03-26 17:19:26 +0100

    new menu button and modified close button

--- nav.js
+++ nav.js
@@ -1,8 +1,12 @@
-function openNav() {
+async function openNav() {
+    document.getElementById("navL").style.width = "250px";
+    await sleep(140);
     document.getElementById("nav").style.width = "250px";
 }
-function closeNav() {
+async function closeNav() {
     document.getElementById("nav").style.width = "0";
+    await sleep(50);
+    document.getElementById("navL").style.width = "0";
 }
 
 // javascript animations? refer to animations.js
@@ -13,4 +17,9 @@ function clickLink(rLink) {
     closeNav();
     leaveAnimation();
     window.location.href = rLink;
+}
+
+//slep
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
