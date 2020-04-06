834647b73df0b5db63e36146589bbfb72dde02e5
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-04-06 17:14:09 +0200

    Automated commit

--- php/applescript.php
+++ php/applescript.php
@@ -13,18 +13,23 @@
 	<body>
     <h1>AppleScript URL Maker (<a href="./urlencode.php">URL Encoder</a>)</h1>
     <form action="./applescript.php" method="POST">
-        <textarea name="script" cols="100" rows="80" for='applescript' placeholder="Put your AppleScript here without changes"></textarea><br>
+        <textarea name="script" cols="50" rows="10" for='applescript' placeholder="Put your AppleScript here without changes"></textarea><br>
         <input type="submit" value="Convert" name="formSubmit" for='submit'><br>
     </form>
     <br>
 	</body>
 </html>
 
-<?php
-        error_reporting(0);
-        $txt = $_POST["script"];
+<?php 
         function urlEncoding($contents) {
+            echo ("Your script:");
             echo ("<a href=\"applescript://com.apple.scripteditor?action=new&script=" . urlencode($contents) . "\">applescript://com.apple.scripteditor?action=new&script=" . urlencode($contents) . "</a>");
         }
-        urlEncoding($txt);
+        if (!empty($_GET)){
+            $txt = $_GET["script"];
+            urlEncoding($txt);
+        } else if (!empty($_POST)) {
+            $txt = $_POST["script"];
+            urlEncoding($txt);
+        }
 ?>
