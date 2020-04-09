2b7ef60a88ed7a482399deebab7cff3ee60d942c
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-04-09 21:22:51 +0200

    fixed thing

--- php/ideas.php
+++ php/ideas.php
@@ -2,30 +2,20 @@
 <html>
 <head>
 <title>JacksonChen666 - Thanks for idea</title>
-<!-- <script type="text/javascript" src="../javascript.js"></script> -->
 <link rel="stylesheet" type="text/css" href="https://jacksonchen666.github.io/main.css">
 </head>
 <body>
 
-<!-- <h1>processing idea..</h1> -->
-<!-- <button onclick="goBack()">back</button> -->
-
 <?php
-
-$thingy = "ideas";
-$txtExtension = ".txt";
 $txt = $_POST[$thingy];
 
-function redirectTo($URLToRedirect = "") {
+function redirectTo($URLToRedirect) {
 	echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URLToRedirect . '">';
 }
 
 function writeFile($TextToWrite) {
-	$file = fopen($thingy . $txtExtension, "a") or die("can't open file F");
-	fwrite($file, time());
-	fwrite($file, ": ");
-	fwrite($file, $TextToWrite);
-	fwrite($file, "\n\n");
+	$file = fopen("ideas.csv", "a") or die("can't open file F");
+	fwrite($file, time().",{$TextToWrite},\n");
 	fclose($file);
 }
 
@@ -37,7 +27,6 @@ else {
 	writeFile($txt);
 	redirectTo("https://JacksonChen666.github.io/thank-you.html");
 }
-
 ?>
 </body>
 </html>
--- php/questions.php
+++ php/questions.php
@@ -2,31 +2,20 @@
 <html>
 <head>
 <title>JacksonChen666 - Thanks for q</title>
-<!-- <script type="text/javascript" src="../javascript.js"></script> -->
 <link rel="stylesheet" type="text/css" href="https://jacksonchen666.github.io/main.css">
 </head>
 <body>
 
-<!-- <h1>processing q..</h1> -->
-<!-- <button onclick="goBack()">back</button> -->
-
 <?php
-
-$thingy = "questions";
-$extension = ".csv";
 $txt = $_POST[$thingy];
 
-function redirectTo($URLToRedirect = "") {
+function redirectTo($URLToRedirect) {
 	echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URLToRedirect . '">';
 }
 
 function writeFile($TextToWrite) {
-	$file = fopen($thingy . $extension, "a") or die("can't open file F");
-	// fwrite($file, time());
-	// fwrite($file, ": ");
-	// fwrite($file, $TextToWrite);
-	// fwrite($file, "\n\n");
-	fwrite($file, "{time()},{$TextToWrite},\n");
+	$file = fopen("questions.csv", "a") or die("can't open file F");
+	fwrite($file, time().",{$TextToWrite},\n");
 	fclose($file);
 }
 
@@ -38,7 +27,6 @@ else {
 	writeFile($txt);
 	redirectTo("https://JacksonChen666.github.io/thank-you.html");
 }
-
 ?>
 </body>
 </html>
