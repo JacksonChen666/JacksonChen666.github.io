0187f819da1f2f237734a3fcf373598613dee17a
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-04-22 08:33:03 +0200

    asdf

--- php/applescript.php
+++ php/applescript.php
@@ -11,13 +11,47 @@
 		<title>JacksonChen666 - AppleScript URL Encoder</title>
 	</head>
 	<body>
-    <h1>AppleScript URL Maker (<a href="./urlencode.php">URL Encoder</a>)</h1>
+    <h1>AppleScript URL Encoder (<a href="./urlencode.php">URL Encoder</a>)</h1>
     <form action="./applescript.php" method="POST">
-        <textarea name="script" cols="50" rows="10" for='applescript' placeholder="Put your AppleScript here without changes"></textarea><br>
-        <input type="submit" value="Convert" name="formSubmit" for='submit'><br>
+        <textarea name="script" cols="50" rows="10" for='applescript' placeholder="Put your AppleScript here without changes" id="applescriptStuff"></textarea><br>
+        <input type="submit" value="Convert" name="formSubmit" for='submit' id="JSLess"><br>
     </form>
     <br>
+    <p id="ASText"></p>
 	</body>
+    <script>
+        // https://www.abeautifulsite.net/adding-and-removing-elements-on-the-fly-using-javascript
+        function removeElement(elementId) {
+            // Removes an element from the document
+            var element = document.getElementById(elementId);
+            element.parentNode.removeChild(element);
+            return element;
+        }
+        function addElement(parentId, elementTag, elementId, html) {
+            // Adds an element to the document
+            var p = document.getElementById(parentId);
+            var newElement = document.createElement(elementTag);
+            newElement.setAttribute('id', elementId);
+            newElement.innerHTML = html;
+            p.appendChild(newElement);
+        }
+
+        function onLoad(){
+            removeELement("JSLess");
+            addElement("applescriptStuff", "button", "JS", "onclick=\"convertToURLAndAS(\"applescriptStuff\",\"ASText\")\", value=\"Convert\"");
+        }
+        function convertToURLAndAS(inputID,outputID){
+            var input = document.getElementById(inputID).value;
+            var output;
+            var ASPre = "applescript://com.apple.scripteditor?action=new&script="
+            input = encodeURIComponent(input);
+            var appleScriptOutput = ASPre + input
+            output = "<a href=\"" + appleScriptOutput + "\">" + appleScriptOutput + "</a>"
+            document.getElementById(outputID).innerHTML = output;
+            document.getElementById(inputID).value = "";
+        }
+        onLoad();
+    </script>
 </html>
 
 <?php 
