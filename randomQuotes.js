1fd23b01fb255efa5478c0fe538be85e850bf6ef
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-06 12:07:28 +0200

    automatic random quotes

--- randomQuotes.js
+++ randomQuotes.js
@@ -3,3 +3,12 @@ function randomQuotes(quotesList=["Oh hi, thanks for checking in im still a piec
 	document.getElementById(ElementId).innerHTML = "\"" + quote + "\" " + endQuote;
 	return true;
 }
+
+function addQuotesListener(eventWithoutOn, ElementId, quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="-Nobody"){
+	document.getElementById(ElementId).addEventListener(eventWithoutOn, function(){randomQuotes(quotesList, endQuote)});
+}
+
+function constantRandomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="-Nobody", ElementId="random_quotes", time=10000){
+	randomQuotes(quotesList, endQuote, ElementId);
+	setInterval(function(){randomQuotes(quotesList, endQuote, ElementId);}, time);
+}
