c90afff7043f43273007219a893ca8ece68711ea
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-06 16:58:16 +0200

    minor change of thing

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,14 +1,14 @@
-function randomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="-Nobody", ElementId="random_quotes"){
+function randomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody", ElementId="random_quotes"){
 	var quote = quotesList[Math.floor(Math.random() * quotesList.length)];
-	document.getElementById(ElementId).innerHTML = "\"" + quote + "\" " + endQuote;
+	document.getElementById(ElementId).innerHTML = "\"" + quote + "\" " + "-" + endQuote;
 	return true;
 }
 
-function addQuotesListener(eventWithoutOn, ElementId, quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="-Nobody"){
+function addQuotesListener(eventWithoutOn, ElementId, quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody"){
 	document.getElementById(ElementId).addEventListener(eventWithoutOn, function(){randomQuotes(quotesList, endQuote)});
 }
 
-function constantRandomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="-Nobody", ElementId="random_quotes", time=10000){
+function constantRandomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody", ElementId="random_quotes", time=10000){
 	randomQuotes(quotesList, endQuote, ElementId);
 	setInterval(function(){randomQuotes(quotesList, endQuote, ElementId);}, time);
 }
