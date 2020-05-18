002827444996d27fb8d71ca0ddf5457e514100f7
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-18 12:19:50 +0200

    lol

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,15 +1,20 @@
-function randomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody", ElementId="random_quotes"){
+defaultQList = ["<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/n0XaSvhTYd4?autoplay=1&controls=0&fs=0&loop=1&modestbranding=1&iv_load_policy=3\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"]
+defaultEQuote = "bill wurtz"
+
+function randomQuotes(quotesList=defaultQList, endQuote=defaultEQuote, ElementId="random_quotes"){
 	var quote = quotesList[Math.floor(Math.random() * quotesList.length)];
 	document.getElementById(ElementId).innerHTML = "\"" + quote + "\" " + "-" + endQuote;
 	return true;
 }
 
-function addQuotesListener(eventWithoutOn, ElementId, quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody"){
+function addQuotesListener(eventWithoutOn, ElementId, quotesList=defaultQList, endQuote=defaultEQuote){
 	document.getElementById(ElementId).addEventListener(eventWithoutOn, function(){randomQuotes(quotesList, endQuote)});
-	document.getElementById(ElementId).style.cursor = "pointer"; // yes, it is clickable from now own
+	document.getElementById(ElementId).style.cursor = "pointer"; // yes, it is clickable from now on
 }
 
-function constantRandomQuotes(quotesList=["Oh hi, thanks for checking in im still a piece of garbage"], endQuote="Nobody", ElementId="random_quotes", time=10000){
+function constantRandomQuotes(quotesList=defaultQList, endQuote=defaultEQuote, ElementId="random_quotes", time=10){
 	randomQuotes(quotesList, endQuote, ElementId);
-	setInterval(function(){randomQuotes(quotesList, endQuote, ElementId);}, time);
+	if (quotesList != defaultQList && endQuote != defaultEQuote){
+		setInterval(function(){randomQuotes(quotesList, endQuote, ElementId);}, time * 1000);
+	}
 }
