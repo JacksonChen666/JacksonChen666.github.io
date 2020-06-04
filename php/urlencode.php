2dc709f3dec1db2467b598d4af88106bc525d4b1
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-06-04 13:38:15 +0200

    changes. now only with 1 php file and working questions and secondary website

--- php/urlencode.php
+++ php/urlencode.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="https://jacksonchen666.github.io/main.css">
-		<meta description="">
-		<meta keywords="">
+        <meta description="">
+        <meta keywords="">
         <style>
-            body * {margin-left: 5px;}
+            body *{margin-left:5px;}
         </style>
-		<title>JacksonChen666 - URL Encoder</title>
-	</head>
-	<body>
+        <title>JacksonChen666 - URL Encoder</title>
+    </head>
+    <body>
     <h1>URL Encoder</h1>
-    <form action="./urlencode.php" method="POST">
+    <form action="." method="POST">
         <textarea name="encode" cols="100" rows="40" for='urlencode' placeholder="Enter text"></textarea><br>
         <input type="submit" value="Convert" name="formSubmit" for='submit'><br>
     </form>
     <br>
-	</body>
+    </body>
 </html>
 
 <?php
-        error_reporting(0);
-        $txt = $_POST["encode"];
-        function urlEncoding($contents) {
-            echo (urlencode($contents));
-        }
-        urlEncoding($txt);
+    error_reporting(0);
+    $txt = $_POST["encode"];
+    function urlEncoding($contents) {
+        echo (urlencode($contents));
+    }
+    urlEncoding($txt);
 ?>
