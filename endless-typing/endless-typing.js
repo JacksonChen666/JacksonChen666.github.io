758c49620a8d7384c0418f5dea42265e721a95f1
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-11 12:32:42 +0100

    something that works (expect more)

--- endless-typing/endless-typing.js
+++ endless-typing/endless-typing.js
@@ -1,3 +1,7 @@
+let inputBox = document.querySelector("#input");
+let toTypeText = document.querySelector("#toType");
+let wordsAmount = 10;
+
 function getWords() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt", false);
@@ -7,15 +11,27 @@ function getWords() {
 
 let words = getWords();
 
-function randomWords() {
-    var wordsChoice = [];
-    while (wordsChoice.join(" ").length != 100) {
-        if (wordsChoice.join(" ").length > 100) {
-            wordsChoice.pop();
-        }
+function randomWords(existing) {
+    var wordsChoice = existing;
+    while (wordsChoice.length != wordsAmount) {
         wordsChoice.push(words[parseInt(Math.random() * words.length + 1)]);
     }
     return wordsChoice.join(" ");
 }
 
-document.querySelector("#toType").innerText = randomWords();
+toTypeText.innerText = randomWords([]);
+inputBox.addEventListener('keyup', e => {
+    if (e.key === " ") {
+        var temp = toTypeText.innerText.split(" ");
+        var wordsTyped = inputBox.value.split(" ");
+        for (i in wordsTyped) {
+            if (temp[i] == wordsTyped[i]) {
+                temp.shift();
+                wordsTyped.shift();
+            }
+        }
+        toTypeText.innerText = randomWords(temp);
+        inputBox.value = wordsTyped.join(" ");
+    }
+    console.log(`keyup event fired for key: ${e.key}`);
+});
