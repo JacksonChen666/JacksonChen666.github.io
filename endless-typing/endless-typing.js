236bdb6a6c1d1ebc70cdc5492d2f601585a4c1bb
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-11 10:45:13 +0100

    some extra stuff to use the words and styling

--- endless-typing/endless-typing.js
+++ endless-typing/endless-typing.js
@@ -1,7 +1,21 @@
 function getWords() {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt", true);
+    xmlHttp.open("GET", "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt", false);
     xmlHttp.send(null);
     return xmlHttp.responseText.split("\n");
 }
-console.log(getWords());
+
+let words = getWords();
+
+function randomWords() {
+    var wordsChoice = [];
+    while (wordsChoice.join(" ").length != 100) {
+        if (wordsChoice.join(" ").length > 100) {
+            wordsChoice.pop();
+        }
+        wordsChoice.push(words[parseInt(Math.random() * words.length + 1)]);
+    }
+    return wordsChoice.join(" ");
+}
+
+document.querySelector("#toType").innerText = randomWords();
