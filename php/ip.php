1150c16421145de3df9c60c720bb6707d8abaced
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-12-04 13:23:28 +0100

    no data required

--- php/ip.php
+++ php/ip.php
@@ -1,20 +1,12 @@
 <?php
-if (isset($_POST['ip'])) {
-    writeFile("IP", $_POST['ip']);
-    http_response_code(204);
-    exit;
+$FileName = "../stuff/" . $Type;
+$exists = file_exists($FileName . ".csv");
+$file = fopen($FileName . ".csv", "a") or die("can't open file F");
+if ($exists == 0) {
+	fwrite($file, "Unix Time, IP Address\n");
 }
-http_response_code(400);
-
-function writeFile($Type, $TextToWrite) {
-	$FileName = "../stuff/" . $Type;
-	$exists = file_exists($FileName . ".csv");
-	$file = fopen($FileName . ".csv", "a") or die("can't open file F");
-	if ($exists == 0) {
-        fwrite($file, "Unix Time, IP Address\n");
-	}
-	fwrite($file, time().",{$TextToWrite}\n");
-	fclose($file);
-	chmod($FileName . ".csv", 0777);
-}
+fwrite($file, time().",{$_SERVER['REMOTE_ADDR']}\n");
+fclose($file);
+chmod($FileName . ".csv", 0777);
+http_response_code(204);
 ?>
