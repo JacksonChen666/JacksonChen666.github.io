76ba19b1d8a1ecdd430e3b5e6195ec3cacc80b8d
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2021-01-28 11:06:50 +0100

    meta change and youtube latest api change

--- youtube-latest.js
+++ youtube-latest.js
@@ -1,20 +1,21 @@
 // Source: https://stackoverflow.com/a/45342740/13104233
 var channelID = "UCut1u2mQy5LMZw5UKgOQKjA";
 var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
-// $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
-//    var link = data.items[0].link;
-//    var id = link.substr(link.indexOf("=")+1);
-// $("#youtube_video").attr("src","https://www.youtube-nocookie.com/embed/"+id + "?showinfo=0&rel=0");
-// });
 
 // Fetching: https://www.codegrepper.com/code-examples/javascript/get+json+data+from+url+javascript
-function getLatestVideo(after) {
-    fetch("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID)
-    .then(res => res.json())
-    .then((data) => {
-        var link = data.items[0].link;
+function getLatestVideoID(after) {
+    getChannelInformation(function(jsonData) {
+        var link = jsonData.items[0].link;
         var id = link.substr(link.indexOf("=")+1);
         after(id);
+    });
+}
+
+function getChannelInformation(after) {
+    fetch("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID)
+    .then(res => res.json())
+    .then((jsonData) => {
+        after(jsonData);
     })
     .catch(err => { throw err });
 }
