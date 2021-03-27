8b52cdc1ad11844bcdd47df755fc2d7754f2e861
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2021-03-27 16:58:38 +0100

    remove useless code instead of cookies, use localStorage for storage removed file:// checking

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
 'use strict';
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setData(name, value) {
+    localStorage.setItem(name, value)
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function getData(name) {
+    localStorage.getItem(name)
 }
 
 function toggleTheme() {
-    var lightMode = (function() { return getCookie("lightTheme"); })();
+    var lightMode = (function() { return getData("lightTheme"); })();
     if (lightMode == "true") {
-        document.body.setAttribute('theme', 'dark');
-        setCookie("lightTheme", false, 10000);
-    } else if (location.protocol == "file:") {
-        // check if user opened it from file system
-        document.body.setAttribute('theme', 'dark');
-        setCookie("lightTheme", false, 10000);
-        var themeButton = document.getElementById("theme");
-        themeButton.parentNode.removeChild(themeButton);
-        console.error("%cThemes don't work properly if you use \"file:\" protocol%c(Opening html from file system, which makes cookies not work)", "color:red;font-size:5rem", "font-size:0.5rem;color:white;");
-        debugger;
+        setData.body.setAttribute('theme', 'dark');
+        setCookie("lightTheme", false);
     } else {
         document.body.setAttribute('theme', 'light');
-        setCookie("lightTheme", true, 10000);
+        setData("lightTheme", true);
     }
     return lightMode;
 }
@@ -48,13 +25,4 @@ function checkTheme() {
     return lightMode;
 }
 
-function requireHTML(HTMLrequired) {
-    setCookie("requireHTML", HTMLrequired, 10000);
-    return HTMLrequired;
-}
-
 window.addEventListener("load", checkTheme);
-if (!(getCookie("requireHTML") == "true" || location.protocol == "file:" || location.host == "localhost" || location.host == "127.0.0.1")) {
-    document.addEventListener("load", antiHTML);
-    document.addEventListener("load", removeHTML);
-}
