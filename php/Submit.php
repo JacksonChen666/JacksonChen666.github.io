a89f21444039b5b60509cc9bdc43a61755a848f5
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2021-03-28 15:16:16 +0200

    change everything lowercase (for jekyll)

--- php/Submit.php
+++ php/Submit.php
@@ -3,35 +3,35 @@ title: Submittion
 layout: external
 ---
 <?php
-	$back = "<a href='javascript:history.back();'>back</a>";
-	if (isset($_POST['q'])) {
-		$submission = writeFile("Question", $_POST['q']);
-	}
-	else if (isset($_POST['i'])) {
-		$submission = writeFile("Idea", $_POST['i']);
-	}
-	else {
-		echo "<h2>tip of the day</h2><p>try and actually type something</p>" . $back;
-		error_log("Submit.php: empty submission found", 0);
-		exit;
-	}
-	echo "<h2>thank</h2><p>i will check</p>" . $back;
+$back = "<a href='javascript:history.back();'>back</a>";
+if (isset($_POST['q'])) {
+	$submission = writeFile("question", $_POST['q']);
+}
+else if (isset($_POST['i'])) {
+	$submission = writeFile("udea", $_POST['i']);
+}
+else {
+	echo "<h2>tip of the day</h2><p>try and actually type something</p>" . $back;
+	error_log("Submit.php: empty submission found", 0);
 	exit;
+}
+echo "<h2>thank</h2><p>i will check</p>" . $back;
+exit;
 
-	function writeFile($Type, $TextToWrite) {
-		$FileName = "../stuff/" . $Type;
-		$exists = file_exists($FileName . ".csv");
-		$file = fopen($FileName . ".csv", "a") or die("can't open file F");
-		if ($exists == 0) {
-			if ($Type == "Question" || $Type == "Idea") {
-				fwrite($file, "Unix Time," . $Type . ",Response\n");
-			} else {
-				fwrite($file, "Unix Time, Data\n");
-			}
+function writeFile($Type, $TextToWrite) {
+	$FileName = "./stuff/" . $Type; 
+	$exists = file_exists($FileName . ".csv");
+	$file = fopen($FileName . ".csv", "a") or die("can't open file F");
+	if ($exists == 0) {
+		if ($Type == "question" || $Type == "idea") {
+			fwrite($file, "unix_time," . $Type . ",response\n");
+		} else {
+			fwrite($file, "unix_time, data\n");
 		}
-		fwrite($file, time().",\"{$TextToWrite}\",\"\"\n");
-		fclose($file);
-		chmod($FileName . ".csv", 0777);
-		return $TextToWrite;
 	}
+	fwrite($file, time().",\"{$TextToWrite}\",\"\"\n");
+	fclose($file);
+	chmod($FileName . ".csv", 0777);
+	return $TextToWrite;
+}
 ?>
